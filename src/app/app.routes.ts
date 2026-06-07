a06408c2a2dd5c16f55f538b5d28a79d57f66417
t-frank-m <ftejena4215@utm.edu.ec>
2026-06-07 17:24:56 -0500

    Work: Ajustes en rutas y componente principal para módulo de pago

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { IterarioComponent } from './modules/iterario/iterario.component';
+import { PagoComponent } from './modules/pago/pago.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: IterarioComponent },
+  { path: 'pago', component: PagoComponent },
+  { path: '**', redirectTo: '' }
+];
