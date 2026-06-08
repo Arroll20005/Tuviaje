9f9f71b2efe301ac6744646b99e151b20fe622b6
t-frank-m <ftejena4215@utm.edu.ec>
2026-06-08 12:28:40 -0500

    Se arreglaron problemas del merge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeContentComponent } from './modules/home/home-content/home-content.component';
 import { SeleccionCooperativasComponent } from './modules/home/pages/seleccion-cooperativas/seleccion-cooperativas.component';
 import { AsientosComponent } from './modules/home/pages/asientos/asientos.component';
+import { PagoComponent } from './modules/pago/pago/pago.component';
 
 export const routes: Routes = [
   {
@@ -9,28 +10,27 @@ export const routes: Routes = [
     redirectTo: 'auth',
     pathMatch: 'full',
   },
-
   {
     path: 'auth',
     loadChildren: () =>
       import('./modules/auth/auth.module').then((m) => m.AuthModule),
   },
-
   {
     path: 'home',
     component: HomeContentComponent,
   },
-
   {
     path: 'seleccion-cooperativas',
     component: SeleccionCooperativasComponent,
   },
-
   {
     path: 'asientos',
     component: AsientosComponent,
   },
-
+  {
+    path: 'pago',
+    component: PagoComponent,
+  },
   {
     path: 'iterario',
     loadComponent: () =>
@@ -38,16 +38,9 @@ export const routes: Routes = [
         (m) => m.IterarioComponent
       ),
   },
-  { 
-    path: 'pago', 
-    loadComponent: () => 
-      import('./modules/pago/pago/pago.component').then(
-        (m) => m.PagoComponent
-      ),
-  },
-  { 
-    path: 'confirmacion', 
-    loadComponent: () => 
+  {
+    path: 'confirmacion-pago',
+    loadComponent: () =>
       import('./modules/pago/pages/confirmacion-pago/confirmacion-pago.component').then(
         (m) => m.ConfirmacionPagoComponent
       ),
