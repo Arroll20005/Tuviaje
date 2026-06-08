87fb8a0ce0689517dde539678282d1699ae9c910
Odalis07 <orodriguez2689 @utm.edu.ec>
2026-06-07 19:25:50 -0500

    Merge de los cambios de src desde main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () =>
-      import('./modules/auth/auth.module').then((m) => m.AuthModule),
-  },
-  {
-    path: 'iterario',
-    loadComponent: () =>
-      import('./modules/iterario/pages/iterario/iterario.component').then(
-        (m) => m.IterarioComponent,
-      ),
-  },
-];
+export const routes: Routes = [];
